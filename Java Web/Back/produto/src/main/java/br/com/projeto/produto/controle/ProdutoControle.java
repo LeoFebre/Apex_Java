bf0d2a30af19228946b9731ce331f3eb98c4ed63
LeoFebre <leocorrea89@outlook.com>
2021-06-25 21:52:52 -0300

    att JavaSpring

--- Java Web/Back/produto/src/main/java/br/com/projeto/produto/controle/ProdutoControle.java
+++ Java Web/Back/produto/src/main/java/br/com/projeto/produto/controle/ProdutoControle.java
@@ -3,6 +3,7 @@ package br.com.projeto.produto.controle;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.projeto.produto.modelo.ProdutoModelo;
 import br.com.projeto.produto.repositorio.ProdutoRepositorio;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ProdutoControle {
 	
